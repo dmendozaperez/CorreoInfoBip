ffd864136d7ae71612c264e009454c95e02285ff
Dmendoza <Dmendoza@DESKTOP-V8CN4C3>
2020-05-11 11:39:41 -0500

    Agregar archivos de proyecto.

---
+++ Email/PostEnvio.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class PostEnvio
+{
+    [Microsoft.SqlServer.Server.SqlFunction()]
+    [System.Security.Permissions.PermissionSet(System.Security.Permissions.SecurityAction.Assert, Unrestricted = true)]
+    public static string send(SqlString from, SqlString to, SqlString subject, SqlString htmlbody, SqlString textbody)
+        {
+        string gRequest = "";
+        try
+        {
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://9r5pjy.api.infobip.com");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", "U2VydmljaW9CYXRhOiQlNTNydjFjMTBCNHQ0JSQ=");
+            var request = new MultipartFormDataContent();
+            request.Add(new StringContent(from.ToString()), "from");
+            request.Add(new StringContent(to.ToString()), "to");
+            request.Add(new StringContent(subject.ToString()), "subject");          
+            request.Add(new StringContent(htmlbody.ToString()), "html");         
+            var response = client.PostAsync("email/2/send", request).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = response.Content;
+                string responseString = responseContent.ReadAsStringAsync().Result;
+                gRequest = responseString;
+
+
+                gRequest = "{\"to\":" + "\"" + to.ToString() + "\"" + "," +
+                               "\"SubmittedAt\":" + "\"" + DateTime.Today.ToString("dd/MM/yyyy") + " " + DateTime.Now.ToLongTimeString() + "\"" + "," +
+                               "\"MessageID\":" + "\"" + "1" + "\"" + "," +
+                               "\"ErrorCode\":" + "\"" + "0" + "\"" + "," +
+                               "\"Message\":" + "\"" + "ok" + "\"" + "}";
+            }
+            else
+            {
+                gRequest = "{\"to\":" + "\"" + to.ToString() + "\"" + "," +
+                              "\"SubmittedAt\":" + "\"" + DateTime.Today.ToString("dd/MM/yyyy") + " " + DateTime.Now.ToLongTimeString() + "\"" + "," +
+                              "\"MessageID\":" + "\"" + "1" + "\"" + "," +
+                              "\"ErrorCode\":" + "\"" + "1" + "\"" + "," +
+                              "\"Message\":" + "\"" + "sin envio de correo" + "\"" + "}";
+            }
+
+           
+        }
+        catch (Exception exc)
+        {
+
+            gRequest = "{\"to\":" + "\"" + to.ToString() + "\"" + "," +
+                           "\"SubmittedAt\":" + "\"" + DateTime.Today.ToString("dd/MM/yyyy") + " " + DateTime.Now.ToLongTimeString() + "\"" + "," +
+                           "\"MessageID\":" + "\"" + "1" + "\"" + "," +
+                           "\"ErrorCode\":" + "\"" + "1" + "\"" + "," +
+                           "\"Message\":" + "\"" + exc.Message + "\"" + "}";
+           
+        }
+        return gRequest;
+    }
+}
+
---
+++ PruebaEnvio/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaEnvio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             PostEnvio.send("bataclub@mk.batamailing.pe", "david.mendoza@bata.com", "Actualización de registro Bata Club", "", "<h1>Html body</h1><p>Rich HTML message body.</p>");            
+
+        }
+    }
+}
